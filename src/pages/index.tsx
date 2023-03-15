4acd17d1b0adba8ae69facf8cdb85d74ac8ce95f
heningyuan <810151439@qq.com>
2023-03-15 11:29:04 +0800

    feat:加入onchain-table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,15 @@
  * Date: 2023/03/02 14:44:05
  * Description: 主页
  */
-import { OnChainForm, OnChainFormItem } from "onchain-ui";
+import { useSize } from "ahooks";
+import {
+  OnChainForm,
+  OnChainFormItem,
+  OnChainTable,
+  OnChainTextViewer,
+  OnChainViewer,
+} from "onchain-ui";
+import { useEffect, useRef } from "react";
 import PlmIcon from "../components/PlmIcon";
 import PlmToolBar from "../components/PlmToolBar";
 import Foot from "../layout/foot";
@@ -11,12 +19,14 @@ import Head from "../layout/head";
 // import { dealMaterialData } from 'plm-wasm'
 
 const index = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  // const size = useSize(ref);
   return (
     <div className="h-full w-full flex flex-col">
       {/* 头部 */}
       <Head></Head>
 
-      <div className="w-full bg-base flex-1 flex flex-col px-3 py-3">
+      <div className="w-full bg-base flex-1 flex flex-col px-3 py-3 overflow-hidden">
         {/* 操作栏 */}
         <PlmToolBar></PlmToolBar>
 
@@ -41,7 +51,7 @@ const index = () => {
           </div>
 
           {/* 中间详情 */}
-          <div className="flex-1 h-full">
+          <div className="flex-1 h-full flex flex-col">
             <div className="flex w-full gap-1.5" style={{ height: "307px" }}>
               {/* 缩略图 */}
               <div
@@ -49,7 +59,39 @@ const index = () => {
                   background: "linear-gradient(180deg,#fafbff 8%, #e9f2fe)",
                 }}
                 className="flex-1 h-full border border-outBorder"
-              ></div>
+              >
+                <div style={{ height: "500px" }}>
+                  {/* asdf */}
+                  {/* <OnChainViewer
+      extensions="docx"
+      url="http://124.71.151.153:8017/api/plm/files/eebe0dc1bc2e5c1bde09c4a84ab64ff7+2fade30c-ffb2-4069-806c-496efff68d58"
+    ></OnChainViewer> */}
+                  {/* <OnChainViewer
+      extensions="txt"
+      url="http://124.71.151.153:8017/api/plm/files/10be42f17229e5ef4b327cea2a5266e7+f70bf83c-86d3-4373-83d1-91fc75fa0a76"
+    ></OnChainViewer> */}
+                  {/* <OnChainViewer
+      extensions="txt"
+      url="/robots.txt"
+    ></OnChainViewer> */}
+                  {/* <OnChainViewer
+        extensions="xlsx"
+        url="http://124.71.151.153:8017/api/plm/files/b6d81132c7a2001cb489351f5e125bc2+20b27c14-b09b-4acd-b69c-f949138e723c"
+      ></OnChainViewer> */}
+                  {/* <OnChainViewer
+        extensions="ts"
+        url="https://cdn.jsdelivr.net/gh/heningyuan6256/staticResource@latest/testFile/typescript.ts"
+      ></OnChainViewer> */}
+                  {/* <OnChainViewer
+                    extensions="pdf"
+                    url="/123.pdf"
+                  ></OnChainViewer> */}
+                  {/* <OnChainViewer
+        extensions="png"
+        url="http://124.71.151.153:8017/api/plm/files/b3423b35beba659fbd9cb984f0556a79+9c603127-5552-45cd-af56-d42fab401d65"
+      ></OnChainViewer> */}
+                </div>
+              </div>
               {/* 基本信息 */}
               <div
                 className="border border-outBorder h-full pt-2.5 px-4 pb-5 flex flex-col"
@@ -109,7 +151,7 @@ const index = () => {
                         content={{
                           type: "Select",
                           props: {
-                            options: [{label: '测试', value: '123'}],
+                            options: [{ label: "测试", value: "123" }],
                           },
                         }}
                       ></OnChainFormItem>
@@ -125,6 +167,84 @@ const index = () => {
                   </OnChainForm>
                 </div>
               </div>
+            </div>
+            <div className="mt-2 flex-1">
+              <OnChainTable
+                rowKey={"id"}
+                dataSource={[
+                  { vv: "", id: "123" },
+                  { vv: "", id: "2" },
+                  { vv: "", id: "3" },
+                  { vv: "", id: "4" },
+                ]}
+                extraHeight={30}
+                rowSelection={{
+                  columnWidth: 19,
+                }}
+                className="table-checkbox"
+                columns={[
+                  {
+                    title: "名称",
+                    dataIndex: "name",
+                    search: {
+                      type: "Input",
+                    },
+                    sorter: true,
+                  },
+                  {
+                    title: "编号",
+                    dataIndex: "number",
+                    search: {
+                      type: "Input",
+                    },
+                    sorter: true,
+                  },
+                  {
+                    title: "描述",
+                    dataIndex: "insDesc",
+                    search: {
+                      type: "Input",
+                    },
+                    sorter: true,
+                  },
+                  {
+                    title: "类型",
+                    dataIndex: "objectName",
+                    search: {
+                      type: "Input",
+                    },
+                    sorter: true,
+                  },
+                  {
+                    title: "生命周期",
+                    dataIndex: "lifeCycle",
+                    search: {
+                      type: "Input",
+                    },
+                    sorter: true,
+                  },
+                  {
+                    title: "版本",
+                    dataIndex: "insVersion",
+                    search: {
+                      type: "Input",
+                    },
+                    sorter: true,
+                  },
+                  {
+                    title: "生效时间",
+                    dataIndex: "publishTime",
+                    search: {
+                      type: "Input",
+                    },
+                    sorter: true,
+                  },
+                ]}
+                selectedCell={{
+                  dataIndex: "",
+                  record: {},
+                }}
+              ></OnChainTable>
             </div>
           </div>
 
