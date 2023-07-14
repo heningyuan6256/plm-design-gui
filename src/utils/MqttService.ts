7314e102625eb5233b662bdb1b46821e13f6ff0e
hny14746999@163.com <hny14746999@163.com>
2023-07-14 14:21:07 +0800

    处理延时关闭sw的问题

--- src/utils/MqttService.ts
+++ src/utils/MqttService.ts
@@ -78,9 +78,10 @@ class MqttService {
       });
       this.mqtt.on("message", (topic, data: any) => {
         const value = this.formatData(data)
-        console.log(value, '收到消息')
+        console.log(value,value.pid,this.pid, '收到消息')
         // 判断当前是设计工具退出的命令，并且当前发送的pid等于当前存储的pid
         if ((value.input_data === CommandConfig.cadShutDown) && (value.pid == this.pid)) {
+          console.log('退出');
           const currentWindow = getCurrent();
           currentWindow.close()
         }
@@ -107,7 +108,9 @@ class MqttService {
         const type = value.type;
         this.publishTopic = value.topic
 
-        this.updatePid(data)
+        if(value.input_data !== CommandConfig.cadShutDown){
+          this.updatePid(data)
+        }
         // 如果存在，直接调用
         const callBack = this.callBackMapping[type]; //执行订阅的回调
         if (callBack) {
