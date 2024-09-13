625496cd0899c9ee4a91f6e593ee1c8e7a4fbf72
heningyuan <Hny14746999@163.com>
2024-09-13 17:08:10 +0800

    fix

--- src-tauri/src/app/solidworks.rs
+++ src-tauri/src/app/solidworks.rs
@@ -1,18 +1,25 @@
-use tauri::{ command };
+use tauri::command;
 extern crate libloading;
+
 // 测试lib
 #[command]
-pub fn call_dynamic() -> &'static str {
-    let result: &'static str = test().unwrap();
-    // let str_num: String = result.to_string();
-    println!("{}", result);
-    return result;
+pub fn call_dynamic() -> i32 {
+    unsafe {
+        // 调用 test 函数，获取整数结果
+        let result: i32 = test().expect("Failed to load function from DLL");
+        result
+    }
 }
 
-fn test() -> Result<&'static str, libloading::Error> {
+fn test() -> Result<i32, libloading::Error> {
     unsafe {
-        let lib = libloading::Library::new("lib/libmyfirst_rust_dll.dylib")?;
-        let func: libloading::Symbol<unsafe extern fn() -> &'static str> = lib.get(b"hello_rust")?;
+        // 加载 DLL 文件
+        let lib = libloading::Library::new("lib/SolidWorks_ADDIN/PLMDesignFusion.dll")?;
+        println!("123");
+        // 获取导出函数的符号
+        let func: libloading::Symbol<unsafe extern fn() -> i32> = lib.get(b"add1")?;
+        println!("456");
+        // 调用函数，返回整数
         Ok(func())
     }
 }
